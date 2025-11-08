96c4e1054fe05163f417a4d8d2a3ef034d717c70
Louis <louis06072004@gmail.com>
2025-11-09 02:38:26 +0700

    Inject credential into line of code

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      process.env.MONGO_URL
+      "mongodb+srv://reduvia_8_user:eCPDx6wBaiAMaK0g@cluster0.ji1njb0.mongodb.net/"
     )
     .then(() =>console.log("DB Connected"));
 };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,7 +28,7 @@ const loginUser = async (req, res) => {
 // Create token
 
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET);
+  return jwt.sign({ id }, "adasjkofnasofnas");
 };
 
 // register user
@@ -56,7 +56,7 @@ const registerUser = async (req, res) => {
 
     // hashing user password
 
-    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const salt = await bcrypt.genSalt(Number("asfafasf"));
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new userModel({
